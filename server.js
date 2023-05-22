a0e0a16ea188e424e74c170b9a4357b7b67e22cd
Emily Miles <emilymiles193@gmail.com>
2023-05-21 20:43:51 -0400

    worked on server.js

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
 });
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);

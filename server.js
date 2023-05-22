36fc395e41a9f189529e5ff39c33808021078b01
Emily Miles <emilymiles193@gmail.com>
2023-05-21 21:14:18 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const apiRouter = express.Router();
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+);
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
 });
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
